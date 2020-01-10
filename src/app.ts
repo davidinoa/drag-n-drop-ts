abcb79f2f63b3ff05c7026eafdcd423b87caf853
David Inoa <davidinoa@gmail.com>
2020-01-10 08:40:34 -0500

    Implement base class for managing state

--- src/app.ts
+++ src/app.ts
@@ -15,24 +15,29 @@ class Project {
 }
 
 // state management
-type Listener = (items: Project[]) => void;
+type Listener<T> = (items: T[]) => void;
 
-class ProjectState {
-  private listeners: Listener[] = [];
+class State<T> {
+  protected listeners: Listener<T>[] = [];
+
+  addListener(listenerFn: Listener<T>) {
+    this.listeners.push(listenerFn);
+  }
+}
+
+class ProjectState extends State<Project> {
   private projects: Project[] = [];
   private static instance: ProjectState;
 
-  private constructor() {}
+  private constructor() {
+    super();
+  }
 
   static getInstance() {
     if (!this.instance) {
       this.instance = new ProjectState();
     }
     return this.instance;
-  }
-
-  addListener(listenerFn: Listener) {
-    this.listeners.push(listenerFn);
   }
 
   addProject(title: string, description: string, numOfPeople: number) {
